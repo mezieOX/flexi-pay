f3261fe2ff7935f9664e31b7942a04b5256650e4
mezieOX <meziepage692@gmail.com>
2024-10-05 05:50:03 +0100

    app updated

--- app/screens/onboarding/onboarding.tsx
+++ app/screens/onboarding/onboarding.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Button} from '../../../components';
 import {Container, StyledImage, TextBackground, StyledText} from './styles';
+
 const OnBoarding = ({navigation}: any) => {
   return (
     <Container>
